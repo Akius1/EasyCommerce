2e58872603c4433427b6620fe84d5f67933598be
Andrew Urom <andrewurom@gmail.com>
2023-03-22 17:36:12 +0100

    Implemented herosection

--- src/Assets/icons/index.ts
+++ src/Assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as DPIconNotification } from './Icon.svg';
 export { ReactComponent as DPIconContent } from './Content.svg';
+export { ReactComponent as DPIconThumbsUp } from './thumbsUp.svg';
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 
 import heroImage from "../Assets/images/Image.png";
-import { DPIconContent } from "../Assets/icons";
+import { DPIconContent, DPIconThumbsUp } from "../Assets/icons";
 
 const HeroSection = () => {
   return (
@@ -20,30 +20,38 @@ const HeroSection = () => {
         image={heroImage}
         title="green iguana"
       />
-      <CardContent sx={{ display: "flex", flexDirection: "row", alignItems: 'center',  }}>
+      <CardContent sx={{ display: "flex", flexDirection: "row", alignItems: 'center', justifyContent: 'space-between'  }}>
         <DPIconContent style={{position:"absolute", top:"120"}} />
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
+            pl:"180px"
           }}
         >
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h6" component="div" >
             Starts in: 3 days : 2 hours : 24 minutes
           </Typography>
           <Box
             sx={{
               display: "flex",
               flexDirection: "row",
+              columnGap:"5px",
             }}
           >
-            <Box>Not Live</Box>
-            <Typography>Layers Auction</Typography>
+            <Typography component={'div'} variant={"body2"} sx={{backgroundColor:"#FFFAEB", color:"#B54708" , py:"2px", px:"8px", }}>Not Live</Typography>
+            <Typography variant={"body2"} sx={{ color:"#475467" , py:"2px", px:"8px", }}>Layers Auction</Typography>
           </Box>
         </Box>
 
         <CardActions>
-          <Button size="small">Share</Button>
+        <Button variant="outlined" sx={{px:"10px", borderColor: "#D0D5DD", color: "#344054", textTransform: "capitalize"}} >
+            <DPIconThumbsUp />
+            <span style={{paddingLeft:"5px"}}>
+            Accept Invite
+            </span>
+            
+        </Button>
         </CardActions>
       </CardContent>
     </Card>
