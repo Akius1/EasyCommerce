60491153959dd46c28a5135995167dc271f52ab2
Andrew Urom <andrewurom@gmail.com>
2023-03-23 12:19:20 +0100

    Added an env file to hide the API

--- src/stores/apiServices.ts
+++ src/stores/apiServices.ts
@@ -1,9 +1,11 @@
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const baseUrl = process.env.REACT_APP_AUCTION_URL;
+console.log(baseUrl)
 
 export const auctionDataApi = createApi({
   reducerPath: 'auctionDataApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://run.mocky.io/v3/' }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getAllData: builder.query({
       query: () => `7f02819f-8254-410a-b8af-ab98572bd26b`,
